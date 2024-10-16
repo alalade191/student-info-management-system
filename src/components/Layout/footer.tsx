b00a8d4680bdcf9667088d4c41b0b40cbcecec5a
alalade191 <alalade191@gmail.com>
2024-10-16 09:13:10 -0700

    responsiveness

--- src/components/Layout/footer.tsx
+++ src/components/Layout/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="bg-[#001A33]">
       <div className="flex lg:flex-row flex-col items-center lg:gap-32 gap-6 px-4 md:px-8 lg:px-20 lg:pt-28 pt-5">
-        <div className="flex flex-col items-center lg:items-start lg:space-y-5 space-y-3">
+        <div className="flex flex-col items-center justify-center lg:justify-normal lg:items-start lg:space-y-5 space-y-3">
           <h3 className="font-semibold text-[32px] text-[#F6F6F6]">Miva</h3>
           <p className="font-normal text-2xl text-[#F6F6F6] max-w-[400px]">
             Miva 2024 All Rights Reserved©
